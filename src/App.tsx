8dc515dd540e39da9d4211cce0b2846bf5ad59da
Oriloo <112803541+Oriloo@users.noreply.github.com>
2026-04-29 16:17:01 +0200

    feat: add ScrollHandler component to manage scroll behavior on route changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
 import HomePage from './pages/HomePage'
 import ProjectPage from './pages/ProjectPage'
+import ScrollHandler from './components/ScrollHandler'
 
 export default function App() {
     const { i18n } = useTranslation()
@@ -12,9 +13,12 @@ export default function App() {
     }, [i18n.language])
 
     return (
-        <Routes>
-            <Route path="/" element={<HomePage/>}/>
-            <Route path="/projects/:id" element={<ProjectPage/>}/>
-        </Routes>
+        <>
+            <ScrollHandler/>
+            <Routes>
+                <Route path="/" element={<HomePage/>}/>
+                <Route path="/projects/:id" element={<ProjectPage/>}/>
+            </Routes>
+        </>
     )
 }
