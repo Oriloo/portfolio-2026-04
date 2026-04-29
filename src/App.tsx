5080027d02c2db93e034d97cc17e4bdaef29aac6
Oriloo <112803541+Oriloo@users.noreply.github.com>
2026-04-29 10:58:22 +0200

    feat: implement internationalization with i18next and add language switcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
 import HomePage from './pages/HomePage'
 import ProjectPage from './pages/ProjectPage'
 
 export default function App() {
+    const { i18n } = useTranslation()
+
+    useEffect(() => {
+        document.documentElement.lang = i18n.language
+    }, [i18n.language])
+
     return (
         <Routes>
             <Route path="/" element={<HomePage/>}/>
